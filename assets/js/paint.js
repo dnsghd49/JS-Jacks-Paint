29d1acb942f4905b6c87a25df00d9bf54e5fe9c4
Aaron Lu <aaronnoh0409@gmail.com>
2021-11-18 23:20:34 -0500

    correct color names and price added

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -16,7 +16,7 @@ function row1() {
     $(document).ready(function () {
         $("#pn1").mouseover(function () {
             $("#pn1").css("opacity", "0.5")
-            appear("white", "$12")
+            appear("Lime Green", "$14.99")
         })
         $("#pn1").mouseout(function () {
             $("#pn1").css("opacity", "1")
@@ -25,7 +25,7 @@ function row1() {
 
         $("#pn2").mouseover(function () {
             $("#pn2").css("opacity", "0.5")
-            appear("white", "$12")
+            appear("Medium Brown", "$11.14")
         })
         $("#pn2").mouseout(function () {
             $("#pn2").css("opacity", "1")
@@ -34,7 +34,7 @@ function row1() {
 
         $("#pn3").mouseover(function () {
             $("#pn3").css("opacity", "0.5")
-            appear("white", "$12")
+            appear("Royal Blue", "$22.99")
         })
         $("#pn3").mouseout(function () {
             $("#pn3").css("opacity", "1")
@@ -47,7 +47,7 @@ function row2() {
     $(document).ready(function () {
         $("#pn4").mouseover(function () {
             $("#pn4").css("opacity", "0.5")
-            appear("white", "$12")
+            appear("Solid Red", "$13.42")
         })
         $("#pn4").mouseout(function () {
             $("#pn4").css("opacity", "1")
@@ -56,7 +56,7 @@ function row2() {
 
         $("#pn5").mouseover(function () {
             $("#pn5").css("box-shadow", "10px 10px 10px 10px #888888")
-            appear("white", "$12")
+            appear("Solid White", "$21.98")
         })
         $("#pn5").mouseout(function () {
             $("#pn5").css("box-shadow", "none")
@@ -65,7 +65,7 @@ function row2() {
 
         $("#pn6").mouseover(function () {
             $("#pn6").css("opacity", "0.5")
-            appear("white", "$12")
+            appear("Solid Black", "$4.99")
         })
         $("#pn6").mouseout(function () {
             $("#pn6").css("opacity", "1")
@@ -78,7 +78,7 @@ function row3() {
     $(document).ready(function () {
         $("#pn7").mouseover(function () {
             $("#pn7").css("opacity", "0.5")
-            appear("white", "$12")
+            appear("Solid Cyan", "$8.22")
         })
         $("#pn7").mouseout(function () {
             $("#pn7").css("opacity", "1")
@@ -87,7 +87,7 @@ function row3() {
 
         $("#pn8").mouseover(function () {
             $("#pn8").css("opacity", "0.5")
-            appear("white", "$12")
+            appear("Solid Purple", "$11.99")
         })
         $("#pn8").mouseout(function () {
             $("#pn8").css("opacity", "1")
@@ -96,7 +96,7 @@ function row3() {
 
         $("#pn9").mouseover(function () {
             $("#pn9").css("opacity", "0.5")
-            appear("white", "$12")
+            appear("Solid Yellow", "$14.99")
         })
         $("#pn9").mouseout(function () {
             $("#pn9").css("opacity", "1")
